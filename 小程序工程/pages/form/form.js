035d95dadd9a1bbbe397b73bf2c5eb4f51e6dbcf
If <247596814@qq.com>
2018-03-29 22:16:49 +0800

    update

---
+++ 小程序工程/pages/form/form.js
@@ -0,0 +1,13 @@
+Page({
+    data: {
+
+    },
+    // 点击提交表单数据
+    formSubmit(e) {
+        console.log(e);
+    },
+    // 
+    checkForm(e) {
+        console.log(e);
+    }
+});
--- 小程序工程/router/index.js
+++ 小程序工程/router/index.js
@@ -1,6 +1,7 @@
 // 路由管理
 const Util = require('../util/index');
 const router = {
+    form: '../form/form',
     loading: '../loading/loading',
     index: '../index/index',
     detail: '../detail/detail',
@@ -13,10 +14,23 @@ const router = {
  * 调用方式 routeTo('targetUrl', { param1: 'xxx', param2: 'yyy' })
  */
 module.exports = {
+    // 正常路由跳转
     routeTo: (url, query) => {
         let params = Util.handleParams(query);
         wx.navigateTo({
             url: router[url] + params
+        });
+    },
+    // 无返回按钮的跳转
+    reLaunchTo: (url) => {
+        wx.reLaunch({
+            url: router[url]
+        });
+    },
+    // 跳转到指定层数
+    navigateBackTo: (index) => {
+        wx.navigateBack({
+            delta: index
         });
     }
 }; 
